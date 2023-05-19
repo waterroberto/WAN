f2d46c1a5a01f618275c2a2616633f5f15f3d291
Samuel Abinemon <wellsfargosloan@gmail.com>
2023-05-19 02:44:58 -0700

    add loan requests

--- pages/account/loan.js
+++ pages/account/loan.js
@@ -6,7 +6,7 @@ import {
   Grid,
   NativeSelect,
 } from "@mui/material";
-import { FormControl } from "@mui/material";
+import { v4 as uuidv4 } from "uuid";
 import LoanHistory from "../../components/dashboard/Loan/LoanHistory";
 import { FaFolderOpen } from "react-icons/fa";
 import React from "react";
@@ -16,19 +16,134 @@ import { useContext, useState } from "react";
 import userDataContext from "../../context/UserDataContext";
 import PopupModal from "../../components/Global/Modal";
 import Container from "../../components/dashboard/Container";
-// import CustomInput from "../../components/UnstyledInput";
 import PrivateRoute from "../../components/auth/PrivateRoute";
+import cogoToast from "cogo-toast";
+
+import { UserService } from "../../services/user";
 
 const Loan = (props) => {
   const [open1, setOpen1] = useState(false);
   const [open2, setOpen2] = useState(false);
+  const [requesting, setRequesting] = useState(false);
   const { userData } = useContext(userDataContext);
+  const [formData, setFormData] = useState({
+    amount: "",
+    duration: "",
+    income: "",
+    placement: "Under Debt Review",
+    employmentStatus: "Permanent Employment",
+    employmentDuration: "",
+    bankStatement: "",
+  });
 
   const handleOpen1 = () => setOpen1(true);
   const handleClose1 = () => setOpen1(false);
 
   const handleOpen2 = () => setOpen2(true);
   const handleClose2 = () => setOpen2(false);
+
+  const {
+    amount,
+    duration,
+    income,
+    placement,
+    employmentStatus,
+    employmentDuration,
+    bankStatement,
+  } = formData;
+
+  const formInputHandler = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const addBankStatement = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      bankStatement: e.target.files[0],
+    }));
+  };
+
+  const bankStatementValid =
+    bankStatement.type === "application/pdf" ||
+    bankStatement.type === "image/jpeg" ||
+    bankStatement.type === "image/png";
+  const amountIsValid = parseInt(amount) >= 5000;
+  const durationIsValid = parseInt(duration) >= 6;
+  const placementIsValid = placement.trim().length > 0;
+  const monthlyIncomIsValid = parseInt(income) > 0;
+  const employmentDurationIsValid = parseInt(employmentDuration) > 0;
+  const employmentStatusIsValid = employmentStatus.trim().length > 0;
+
+  const detailsAreValid =
+    bankStatementValid &&
+    amountIsValid &&
+    durationIsValid &&
+    placementIsValid &&
+    employmentStatusIsValid &&
+    employmentDurationIsValid &&
+    monthlyIncomIsValid;
+
+  const sendLoanRequest = async () => {
+    if (!detailsAreValid) {
+      cogoToast.error("One or more details are invalid");
+      if (!bankStatementValid) {
+        cogoToast.error("Attach valid document");
+      }
+      if (!amountIsValid) {
+        cogoToast.error("Loan amount must be 5000 or higher");
+      }
+      if (!durationIsValid) {
+        cogoToast.error("Loan duration must be at least 6 months");
+      }
+      if (!placementIsValid) {
+        cogoToast.error("Kindly select financial status");
+      }
+      if (!employmentStatusIsValid) {
+        cogoToast.error("Please select employment status");
+      }
+      if (!employmentDurationIsValid) {
+        cogoToast.error("Input employment duration");
+      }
+      if (!monthlyIncomIsValid) {
+        cogoToast.error("Input monthly income");
+      }
+    } else {
+      setRequesting(true);
+      const data = {
+        _id: uuidv4(),
+        _user: userData?.id,
+        ...formData,
+        amount: +amount,
+        income: +income,
+        duration: +duration,
+        employmentDuration: +employmentDuration,
+        bankStatement: "",
+        applicationDate: new Date(),
+      };
+
+      try {
+        const res = await UserService.sendLoanRequest(data, bankStatement);
+
+        if (res.ok) {
+          const pendingLoanRequest = await UserService.addPendingLoan(
+            userData?.id,
+            { amount: +amount, duration: +duration }
+          );
+
+          console.log(pendingLoanRequest);
+          cogoToast.success("Loan sent");
+        }
+
+        setRequesting(false);
+      } catch (error) {
+        console.log(error);
+        setRequesting(false);
+      }
+    }
+  };
 
   return (
     <PrivateRoute>
@@ -45,9 +160,6 @@ const Loan = (props) => {
         title="Loan Application"
         sx={{ maxWidth: "768px" }}
       >
-        <Typography mb={2} fontWeight={300}>
-          Loan Details
-        </Typography>
         <Grid
           container
           mx="auto"
@@ -57,7 +169,9 @@ const Loan = (props) => {
           alignItems="center"
         >
           <Grid item xs={12} sm={6} width="100%">
+            <label htmlFor="duration">Loan Amount</label>
             <input
+              name="amount"
               className="styled-input"
               aria-label="Desired Amount"
               placeholder="Desired Amount"
@@ -68,13 +182,17 @@ const Loan = (props) => {
                 border: "1px solid #555",
                 fontSize: 15,
               }}
+              value={amount}
+              onChange={formInputHandler}
             />
           </Grid>
           <Grid item xs={12} sm={6} width="100%">
+            <label htmlFor="duration">Loan Duration</label>
             <input
+              name="duration"
               className="styled-input"
               aria-label="Repayment Duration"
-              placeholder="Repayment Duration (months)"
+              placeholder="Loan Duration (months)"
               type="number"
               required
               sx={{
@@ -82,12 +200,11 @@ const Loan = (props) => {
                 border: "1px solid #555",
                 fontSize: 15,
               }}
+              value={duration}
+              onChange={formInputHandler}
             />
           </Grid>
         </Grid>
-        <Typography my={2} fontWeight={300}>
-          Applicant`s Details
-        </Typography>
         <Grid
           container
           mx="auto"
@@ -97,7 +214,9 @@ const Loan = (props) => {
           alignItems="center"
         >
           <Grid item xs={12} sm={6} width="100%">
+            <label htmlFor="income">Monthly Income</label>
             <input
+              name="income"
               className="styled-input"
               aria-label="Monthly Income"
               placeholder="Monthly Income"
@@ -108,79 +227,74 @@ const Loan = (props) => {
                 border: "1px solid #555",
                 fontSize: 15,
               }}
+              value={income}
+              onChange={formInputHandler}
             />
           </Grid>
           <Grid item xs={12} sm={6} width="100%">
-            <Typography mt={{ xs: 2, sm: -6 }} mb={1} fontSize={14}>
-              Financial Placement
-            </Typography>
-            <FormControl fullWidth>
-              <NativeSelect
-                defaultValue={"Under Debt Review"}
-                inputProps={{
-                  name: "financial-placement",
-                  id: "financial-placement",
-                }}
-                sx={{
-                  width: "100%",
-                  color: "#ccc",
-                  padding: "0.75rem 1rem",
-                  borderRadius: "10px",
-                  fontSize: "14px",
-                  color: "#ccc",
-                  background: "var(--dark)",
-                  border: "1px solid #555",
-                  fontSize: 15,
+            <label htmlFor="financial-placement">Financial Placement</label>
+            <NativeSelect
+              defaultValue={placement}
+              inputProps={{
+                name: "placement",
+                id: "financial-placement",
+              }}
+              sx={{
+                width: "100%",
+                color: "#ccc",
+                padding: "0.75rem 1rem",
+                borderRadius: "10px",
+                fontSize: "14px",
+                color: "#ccc",
+                background: "var(--dark)",
+                border: "1px solid #555",
+                fontSize: 15,
 
-                  "&:before, &:after": {
-                    display: "none",
-                  },
-                }}
-              >
-                <option value="Unpaid Loan">Unpaid Loan</option>
-                <option value="Bad Credit Score">Bad Credit Score</option>
-                <option value="Under Debt Review">Under Debt Review</option>
-                <option value="Blacklisted">Blacklisted</option>
-                <option value="None">None</option>
-              </NativeSelect>
-            </FormControl>
+                "&:before, &:after": {
+                  display: "none",
+                },
+              }}
+              onChange={formInputHandler}
+            >
+              <option value="Unpaid Loan">Unpaid Loan</option>
+              <option value="Bad Credit Score">Bad Credit Score</option>
+              <option value="Under Debt Review">Under Debt Review</option>
+              <option value="Blacklisted">Blacklisted</option>
+              <option value="None">None</option>
+            </NativeSelect>
           </Grid>
           <Grid item xs={12} sm={6} width="100%">
-            <Typography mt={{ xs: 2, sm: 0 }} mb={1} fontSize={14}>
-              Employment Status
-            </Typography>
-            <FormControl fullWidth>
-              <NativeSelect
-                defaultValue={"Permanent Employment"}
-                inputProps={{
-                  name: "employment-status",
-                  id: "employment-status",
-                }}
-                sx={{
-                  width: "100%",
-                  color: "#ccc",
-                  padding: "0.75rem 1rem",
-                  borderRadius: "10px",
-                  fontSize: "14px",
-                  color: "#ccc",
-                  background: "var(--dark)",
-                  border: "1px solid #555",
-                  fontSize: 15,
+            <label htmlFor="employmentStatus">Financial Status</label>
+            <NativeSelect
+              defaultValue={employmentStatus}
+              inputProps={{
+                name: "employmentStatus",
+                id: "employmentStatus",
+              }}
+              sx={{
+                width: "100%",
+                color: "#ccc",
+                padding: "0.75rem 1rem",
+                borderRadius: "10px",
+                fontSize: "14px",
+                color: "#ccc",
+                background: "var(--dark)",
+                border: "1px solid #555",
+                fontSize: 15,
 
-                  "&:before, &:after": {
-                    display: "none",
-                  },
-                }}
-              >
-                <option value="Permanent Employment">
-                  Permanent Employment
-                </option>
-                <option value="Contract Based">Contract Based</option>
-                <option value="Self Employed">Self Employed</option>
-              </NativeSelect>
-            </FormControl>
+                "&:before, &:after": {
+                  display: "none",
+                },
+              }}
+              onChange={formInputHandler}
+            >
+              <option value="Permanent Employment">Permanent Employment</option>
+              <option value="Contract Based">Contract Based</option>
+              <option value="Self Employed">Self Employed</option>
+            </NativeSelect>
           </Grid>
           <Grid item xs={12} sm={6} width="100%">
+            <label htmlFor="employmentDuration">Employment Duration</label>
             <input
               className="styled-input"
               aria-label="Employment Duration"
@@ -193,39 +307,43 @@ const Loan = (props) => {
                 fontSize: 15,
                 mt: { xs: 2, sm: 5 },
               }}
+              name="employmentDuration"
+              value={employmentDuration}
+              onChange={formInputHandler}
             />
           </Grid>
-          <Grid item xs={12} sm={6} width="100%">
-            <Button
-              variant="text"
-              disableElevation
-              sx={{
-                p: 1.5,
-                color: "#fff",
-                textTransform: "capitalize",
-                fontFamily: "inherit",
-                background: "var(--blue)",
-                transition: "0.5s ease-in",
-                borderRadius: 1,
+          <Button
+            variant="text"
+            disableElevation
+            sx={{
+              mt: 2,
+              p: 2,
+              color: "#fff",
+              textTransform: "capitalize",
+              fontFamily: "inherit",
+              border: "2px solid var(--blue)",
 
-                "&:hover": {
-                  transition: "0.5s ease-out",
-                  background: "var(--blue-hover)",
-                },
-                width: "100%",
-              }}
-              onClick={() => {}}
-              component="label"
-            >
-              Upload Recent Bank Statement (3 - 6 months)
-              <input
-                hidden
-                accept=".png, .jpg, .jpeg, .pdf"
-                multiple
-                type="file"
-              />
-            </Button>
-          </Grid>
+              "&:hover": {
+                border: "2px solid var(--blue-hover)",
+              },
+              width: "100%",
+            }}
+            onClick={() => {}}
+            component="label"
+          >
+            {bankStatement
+              ? "Change Document"
+              : "Upload Recent Bank Statement(3 months)"}{" "}
+            {" - "}
+            <input
+              hidden
+              accept=".png, .jpg, .jpeg, .pdf"
+              multiple
+              type="file"
+              onChange={addBankStatement}
+            />
+            {bankStatement && bankStatement.name}
+          </Button>
         </Grid>
         <Button
           disableElevation
@@ -243,9 +361,10 @@ const Loan = (props) => {
             },
             width: "100%",
           }}
-          onClick={() => {}}
+          onClick={sendLoanRequest}
+          disabled={requesting}
         >
-          Proceed
+          {requesting ? "Loading..." : "Proceed"}
         </Button>
       </PopupModal>
       <Box minHeight="100vh" sx={{ background: "var(--darker)" }}>
---
+++ services/user.js
@@ -0,0 +1,63 @@
+import {
+  doc,
+  getDoc,
+  getFirestore,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { ref, getDownloadURL, uploadBytes, getStorage } from "firebase/storage";
+import { db, storage } from "./firebase.config";
+
+export const UserService = {
+  createNewUser: async function (email, password) {
+    return { email, password };
+  },
+
+  getUrlFromFileUpload: async function (userId, loanId, file) {
+    const fileRef = ref(
+      storage,
+      `bankStatements/${userId}__${loanId}__${new Date().getTime()}`
+    );
+
+    const snapshot = await uploadBytes(fileRef, file);
+    const fileUrl = await getDownloadURL(snapshot.ref);
+
+    return fileUrl;
+  },
+
+  sendLoanRequest: async function (data, file) {
+    const url = await this.getUrlFromFileUpload(data._user, data._id, file);
+
+    await setDoc(doc(db, "loanRequests", data?._id), {
+      ...data,
+      bankStatement: url,
+    });
+
+    return { message: "Successful", ok: true };
+  },
+
+  addPendingLoan: async function (userId, data) {
+    const userRef = doc(db, "users", userId);
+    const res = await getDoc(userRef);
+
+    const userLoans = res.data().loans;
+
+    await updateDoc(userRef, {
+      loans: [
+        ...userLoans,
+        {
+          ...data,
+          type: "loan",
+          status: "pending",
+          application_date: new Date(),
+          payout_date: ". . .",
+          repayment_date: ". . .",
+        },
+      ],
+    });
+
+    return {
+      userId,
+    };
+  },
+};
